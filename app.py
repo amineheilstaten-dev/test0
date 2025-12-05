da68770da31fa849eebfbecec6b1830dbc7f9b8e
amineheilstaten-dev <amineheilstaten@gmail.com>
2025-12-05 19:21:02 +0100

    Change home response and update app run settings
    Updated home route response and modified run configuration for deployment.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Render! My Python App is working."
+    return "Hello World. I am trying to publish my siteweb on render.com"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    # هاد السطر كيعني أن التطبيق غيخدم غير فالبيسي ديالك للتجربة
+    # Render ما كيخدمش بهاد السطر، هو كيستعمل Gunicorn
+    app.run(host='0.0.0.0', port=5000, debug=True)
