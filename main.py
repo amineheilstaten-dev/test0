a4df983fdcc30099f47fd48eeef23c8826bc6210
amineheilstaten-dev <amineheilstaten@gmail.com>
2025-12-05 16:44:56 +0100

    Convert script to Flask web application

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello Yassine in Github !")
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Render! My Python App is working."
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
